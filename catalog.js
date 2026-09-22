b886e5b7ce42bbff9c37ddc650f301219e8d8b63
DennisHaus <156682471+DennisHaus@users.noreply.github.com>
2026-09-22 13:07:44 +0200

    cc

--- catalog.js
+++ catalog.js
@@ -2,14 +2,14 @@
   "version": 1,
   "scans": [
     {
-      "id": "warehouse-001",
-      "name": "Warehouse Exterior",
+      "id": "output-001",
+      "name": "Output Test",
       "filename": "output.copc.laz",
       "path": "scans/output.copc.laz",
       "url": "https://dennishaus.github.io/pointcloud_viewer/scans/output.copc.laz",
       "sizeBytes": 73400320,
       "pointCount": 12800000,
-      "crs": "EPSG:26910",
+      "crs": "",
       "uploadedAt": "2026-09-21"
     }
   ]
